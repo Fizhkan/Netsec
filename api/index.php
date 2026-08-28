e37f721121e784676f6ebe74c582177edb0ee5d7
sirajd3itb <sirajd3itb@gmail.com>
2026-08-28 23:31:45 +0700

    feat: configure Laravel storage and cache directories for Vercel serverless environment

--- api/index.php
+++ api/index.php
@@ -1,10 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-// Buat folder storage dinamis di /tmp
+// Siapkan folder storage di /tmp
 $dirs = [
     '/tmp/storage/framework/views',
     '/tmp/storage/framework/cache',
@@ -20,19 +16,5 @@ foreach ($dirs as $dir) {
     }
 }
 
-// Override storage dan cache path ke /tmp
-$_ENV['APP_STORAGE'] = '/tmp/storage';
-$_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
-$_ENV['APP_CONFIG_CACHE'] = '/tmp/bootstrap/cache/config.php';
-$_ENV['APP_EVENTS_CACHE'] = '/tmp/bootstrap/cache/events.php';
-$_ENV['APP_PACKAGES_CACHE'] = '/tmp/bootstrap/cache/packages.php';
-$_ENV['APP_ROUTES_CACHE'] = '/tmp/bootstrap/cache/routes.php';
-$_ENV['APP_SERVICES_CACHE'] = '/tmp/bootstrap/cache/services.php';
-
-require __DIR__ . '/../vendor/autoload.php';
-
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-$app->useStoragePath('/tmp/storage');
-
-$app->handleRequest(Request::capture());
+// Forward request langsung ke file public/index.php Laravel
+require __DIR__ . '/../public/index.php';
